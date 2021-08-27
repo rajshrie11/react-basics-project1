d5ce5337f08710ee79914898546f30ded44d5e7e
MyGitHub-11 <74892310+MyGitHub-11@users.noreply.github.com>
2021-08-27 15:04:35 +0530

    First commit

---
+++ react-basics-project/src/App.js
@@ -0,0 +1,17 @@
+
+import Todo from './components/Todo';
+
+
+function App() {
+  return( 
+  <div>
+    <h1>My todos</h1>
+    <Todo text='Learn Recat'/>
+    <Todo text='Master React' />
+    <Todo text='Expert in React' />
+    
+  </div>
+  );
+}
+
+export default App;
